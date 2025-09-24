0067f29ad48cc022548f0f5a55abadade95a6aab
schorts99 <schorts99@gmail.com>
2025-09-24 11:13:40 +0000

    build: Add initial config

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+  // jest.config.js
+  const { defaults: tsjPreset } = require("ts-jest/presets");
+const { transform } = require("typescript");
+
+  module.exports = {
+    preset: "ts-jest/presets/default-esm",
+    testEnvironment: "node",
+    extensionsToTreatAsEsm: [".ts"],
+    transform: {
+      '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
+    },
+    moduleFileExtensions: ["ts", "js", "json", "node"],
+    testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+    collectCoverageFrom: [
+      "src/**/*.ts",
+      "!src/**/*.d.ts",
+      "!src/index.ts",
+    ],
+    coverageDirectory: "coverage",
+    coverageReporters: ["text", "lcov", "html"],
+    moduleNameMapper: {
+      "^@schorts99/shared-kernel/(.*)$": "<rootDir>/src/$1",
+    },
+  };
+  
