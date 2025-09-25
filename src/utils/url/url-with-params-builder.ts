81e89ecca0e727ea41099bed5a23ad742b43b563
schorts99 <schorts99@gmail.com>
2025-09-25 18:54:34 +0000

    refactor(utils): Improve URLWithParamsBuilder.

--- src/utils/url/url-with-params-builder.ts
+++ src/utils/url/url-with-params-builder.ts
@@ -1,12 +1,12 @@
 export class URLWithParamsBuilder {
   constructor(private readonly base: URL) {}
 
-  with(params: Record<string, string | number | boolean | string[] | number[]>): URLWithParamsBuilder {
+  with(params: Record<string, string | number | boolean | Array<string | number>>): URLWithParamsBuilder {
     Object.entries(params).forEach(([key, value]) => {
       if (Array.isArray(value)) {
-        value.forEach(v => this.base.searchParams.append(key, JSON.stringify(v)));
+        value.forEach(v => this.base.searchParams.append(key, String(v)));
       } else {
-        this.base.searchParams.set(key, JSON.stringify(value));
+        this.base.searchParams.set(key, String(value));
       }
     });
 
