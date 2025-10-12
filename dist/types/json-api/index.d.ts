a22ff8f466fee007cd46a7990f991ed1f35219eb
schorts99 <schorts99@gmail.com>
2025-10-12 18:57:31 +0000

    fix(json-api): Added missed export for `JSONAPIErrors`.

--- dist/types/json-api/index.d.ts
+++ dist/types/json-api/index.d.ts
@@ -2,4 +2,5 @@ export type { JSONAPISingle } from "./json-api-single";
 export type { JSONAPIList } from "./json-api-list";
 export { URLCriteriaBuilder } from "./url-criteria-builder";
 export { JSONAPIConnector } from "./json-api-connector";
+export * from "./exceptions";
 //# sourceMappingURL=index.d.ts.map
--- src/json-api/index.ts
+++ src/json-api/index.ts
@@ -2,3 +2,4 @@ export type { JSONAPISingle } from "./json-api-single";
 export type { JSONAPIList } from "./json-api-list";
 export { URLCriteriaBuilder } from "./url-criteria-builder";
 export { JSONAPIConnector } from "./json-api-connector";
+export * from "./exceptions";
