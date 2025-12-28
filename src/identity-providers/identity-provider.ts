2a109b967c46575ed1779b585b0a78fae0916e37
Jorge Castillo <jorge.castillo@rankmi.com>
2025-12-28 13:33:35 -0600

    feat: Update `IdentityProvider`.

--- src/identity-providers/identity-provider.ts
+++ src/identity-providers/identity-provider.ts
@@ -1,5 +1,5 @@
 export interface IdentityProvider<IdentityUser> {
-  createUser(): Promise<{ providerId: string }>;
+  createUser(...args: any[]): Promise<IdentityUser>;
   verifyToken(token: string): Promise<{ userId: string; claims: Record<string, any> }>;
   getUser(userId: string): Promise<IdentityUser>;
   getUsers(userIds: Array<string>): Promise<IdentityUser[]>;
