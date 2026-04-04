bbc1f436d3b333fbb299db6598672c8f07d7a6bd
Jorge Castillo <schorts@outlook.com>
2026-04-04 08:33:58 -0600

    feat: Add the possibility to be optional on `StringValue`

--- src/value-objects/string-value.ts
+++ src/value-objects/string-value.ts
@@ -1,25 +1,42 @@
 import { ValueObject } from "./";
 
-export abstract class StringValue implements ValueObject {
+export abstract class StringValue<Optional extends boolean = false> implements ValueObject {
   readonly valueType = "String";
-  readonly value: string;
+  readonly value: Optional extends true ? string | null : string;
   readonly minLength: number;
   readonly maxLength: number | undefined;
+  readonly optional: Optional;
 
-  constructor(value: string, minLength = 0, maxLength?: number) {
+  constructor(
+    value: Optional extends true ? string | null : string,
+    minLength = 0,
+    maxLength?: number,
+    optional: Optional = false as Optional
+  ) {
     this.value = value;
     this.minLength = minLength;
     this.maxLength = maxLength;
+    this.optional = optional;
   }
 
   get isValid(): boolean {
-    return this.value.length >= this.minLength && (this.maxLength ? this.value.length <= this.maxLength : true);
+    if (this.optional && this.value === null) {
+      return true;
+    }
+
+    if (this.value === null) {
+      return false;
+    }
+
+    const length = this.value.length;
+
+    return length >= this.minLength && (this.maxLength ? length <= this.maxLength : true);
   }
 
   equals(valueObject: unknown): boolean {
     if (!(valueObject instanceof StringValue)) return false;
     if (!this.isValid || !valueObject.isValid) return false;
-  
+
     return this.value === valueObject.value;
   }
 
